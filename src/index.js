4353fd39c301d71e7b16077830d2df1ad5dff30e
GHS\zcd17a <zdunn@ccihealth.org>
2017-09-14 15:13:10 -0400

    Use one mongo collection

--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ const ObjectID = mongo.ObjectID;
 const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_URL = 'mongodb://api:password@ds135444.mlab.com:35444/restfest-2017-hackathon';
-const COLLECTIONS = {
-    hospitals: 'hospitals',
-    measures: 'measures'
-};
+const COLLECTION = 'hospitals';
 let db; // will be assigned later
 
 function returnResults(res) {
@@ -27,13 +24,23 @@ app.get('/', (req, res) => {
 });
 
 app.get('/hospitals', (req, res) => {
-    db.collection(COLLECTIONS.hospitals).find()
+    db.collection(COLLECTION).find()
         .toArray(returnResults(res));
 });
 
 app.get('/hospitals/:id', (req, res) => {
-    db.collection('hospitals').findOne({ _id: new ObjectID(req.params.id) })
+    db.collection(COLLECTION).findOne({ _id: new ObjectID(req.params.id) })
         .then(hospital => res.json(hospital));
+});
+
+app.get('/measures', (req, res) => {
+    db.collection(COLLECTION).find()
+        .toArray(returnResults(res));
+})
+
+app.get('/measures/:id', (req, res) => {
+    db.collection(COLLECTION).findOne({ _id: new ObjectID(req.params.id) })
+        .then(measure => res.json(measure));
 });
 
 MongoClient.connect(MONGO_URL, (err, database) => {
