1183f0709e1897e7f2a6bf12310617bc8772f72c
GHS\zcd17a <zdunn@ccihealth.org>
2017-09-14 14:07:09 -0400

    Fix hospital id endpoint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
-var MongoClient = require('mongodb').MongoClient;
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
+const ObjectID = mongo.ObjectID;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_URL = 'mongodb://api:password@ds135444.mlab.com:35444/restfest-2017-hackathon';
+const COLLECTIONS = {
+    hospitals: 'hospitals',
+    measures: 'measures'
+};
 let db; // will be assigned later
 
 function returnResults(res) {
@@ -21,13 +27,14 @@ app.get('/', (req, res) => {
 });
 
 app.get('/hospitals', (req, res) => {
-    db.collection('hospitals').find().toArray(returnResults(res));
+    db.collection(COLLECTIONS.hospitals).find()
+        .toArray(returnResults(res));
 });
 
 app.get('/hospitals/:id', (req, res) => {
-    const hospital = db.collection('hospitals').findOne({ _id: req.params.id });
-    res.json(hospital);
-})
+    db.collection('hospitals').findOne({ _id: new ObjectID(req.params.id) })
+        .then(hospital => res.json(hospital));
+});
 
 MongoClient.connect(MONGO_URL, (err, database) => {
     if (err) console.error(err);
