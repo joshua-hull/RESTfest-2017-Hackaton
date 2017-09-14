fde3c455171ee646e7998051543de99008e314d3
GHS\zcd17a <zdunn@ccihealth.org>
2017-09-14 10:48:00 -0400

    Move Hospital model Add Measure model

---
+++ src/models/Measure.js
@@ -0,0 +1,8 @@
+class Measure {
+    constructor({ id, name, rating, responseRate }) {
+        this.id = id;
+        this.name = name;
+        this.score = score;
+        this.responseRate = responseRate;
+    }
+}
