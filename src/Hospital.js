8ff872332a237bfd4d0ebc78faf81d372863e980
GHS\zcd17a <zdunn@ccihealth.org>
2017-09-14 10:25:46 -0400

    Add initial hospital model

---
+++ src/Hospital.js
@@ -0,0 +1,12 @@
+class Hospital {
+    constructor({ id, name, address, city, state, zip, county, phone }) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.county = county;
+        this.phone = phone;
+    }
+}
