76e7b2bba17e9e92cabc957712d18990df476107
GHS\zcd17a <zdunn@ccihealth.org>
2017-09-14 13:06:31 -0400

    Add db username/password

--- src/index.js
+++ src/index.js
@@ -3,23 +3,28 @@ var MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGO_URL = process.env.MONGO_URL || 'mongodb://<dbuser>:<dbpassword>@ds135444.mlab.com:35444/restfest-2017-hackathon';
+const MONGO_URL = 'mongodb://api:password@ds135444.mlab.com:35444/restfest-2017-hackathon';
 let db; // will be assigned later
+
+function returnResults(err, results) {
+    if (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
+    res.json(results);
+}
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
 app.get('/hospitals', (req, res) => {
-    db.collection('hospitals').find()
-        .toArray((err, hospitals) => {
-            if (err) {
-                console.error(err);
-                res.status(500).send(err);
-            }
-            res.json(hospitals);
-        });
+    db.collection('hospitals').find().toArray(returnResults);
 });
+
+app.get('/hospitals/:id', (req, res) => {
+    db.collection('hospitals').find(id).toArray(returnResults);
+})
 
 MongoClient.connect('mongo-url', (err, database) => {
     if (err) console.error(err);
