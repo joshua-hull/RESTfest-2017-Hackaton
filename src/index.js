c58b395e4e43c3fc923674810be843679b6485ec
GHS\zcd17a <zdunn@ccihealth.org>
2017-09-14 12:25:56 -0400

    Add mongo dependency

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
+});
+
+app.get('/hospitals', (req, res) => {
+    // res.json()
 });
 
 app.listen(PORT, () => {
